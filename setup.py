ae83f54d7afa840cbdeb18ec362eb6df2a70285c
Yurzs <yurzs@icloud.com>
2019-09-13 02:42:51 +0300

    pypi description

--- setup.py
+++ setup.py
@@ -1,12 +1,19 @@
 from distutils.core import setup
 import datetime
 
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name='neptune_resolver_rest',  # How you named your package folder (MyLib)
     packages=['neptune_resolver_rest'],  # Chose the same as "name"
     version=datetime.datetime.now(tz=datetime.timezone.utc).strftime('%Y.%m.%d.%H%M'),  # Start with a small number and increase it with every change you make
     license='MIT',  # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-    description='HTTP Request based resolver',  # Give a short description about your library
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    description='HTTP Request based resolver for NEPTUNE DNS SERVER',  # Give a short description about your library
     author='Yury (Yurzs)',  # Type in your name
     author_email='dev@best-service.online',  # Type in your E-Mail
     url='https://git.best-service.online/yurzs/neptune-resolver-rest',  # Provide either the link to your github or to your website
